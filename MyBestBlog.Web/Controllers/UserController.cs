c24255fd2b0f1b3f6bf6d5f6b736ac9163918fc2
CS-SF-Kon <vladislav.konovalov@evraz.com>
2025-06-30 23:18:39 +0700

    day 4 of frontend settings: user profile view that displays few info prepared, allowed for editing only for admin and current user

---
+++ MyBestBlog.Web/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyBestBlog.Core.Entities;
+using MyBestBlog.Core.Interfaces;
+using MyBestBlog.Web.Models;
+using System.Security.Claims;
+
+namespace MyBestBlog.Web.Controllers;
+
+public class UserController : Controller
+{
+    private readonly IUserRepository _userRepo;
+    private readonly IArticleRepository _articleRepo;
+    private readonly UserManager<User> _userManager;
+
+    public UserController(
+        IUserRepository userRepo,
+        IArticleRepository articleRepo,
+        UserManager<User> userManager)
+    {
+        _userRepo = userRepo;
+        _articleRepo = articleRepo;
+        _userManager = userManager;
+    }
+    [HttpGet]
+    public async Task<IActionResult> Profile(Guid id)
+    {
+        var user = await _userRepo.GetUserByUserIdAsync(id);
+        if (user == null) return NotFound();
+
+        var currentUserIdClaim = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (!Guid.TryParse(currentUserIdClaim, out var currentUserId))
+        {
+            return Forbid(); // или другая обработка ошибки
+        }
+        var isAdmin = User.IsInRole("Admin");
+
+        var articles = (await _articleRepo.GetArticlesByUserIdAsync(id))
+            .Select(a => new UserArticleViewModel
+            {
+                Id = a.Id,
+                Title = a.Title,
+                Tags = a.Tags.Select(t => t.Tag.Name).ToList()
+            }).ToList();
+
+        var model = new UserProfileViewModel
+        {
+            UserId = user.Id,
+            UserName = user.UserName,
+            DisplayName = user.DisplayName,
+            Email = user.Email,
+            Role = (await _userManager.GetRolesAsync(user)).FirstOrDefault(),
+            RegistrationDate = user.RegistrationDate,
+            Articles = articles,
+            CanEdit = id == currentUserId || isAdmin
+        };
+
+        return View(model);
+    }
+}
---
+++ MyBestBlog.Web/Models/UserProfileViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace MyBestBlog.Web.Models;
+
+public class UserProfileViewModel
+{
+    public Guid UserId { get; set; }
+    public string UserName { get; set; }
+    public string DisplayName { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public List<UserArticleViewModel> Articles { get; set; }
+    public bool CanEdit { get; set; }
+}
+
+public class UserArticleViewModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public List<string> Tags { get; set; }
+}
